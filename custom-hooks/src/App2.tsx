8d9b204b2f8a0d1f058cb5fa70672630c088a68d
wyz <wyz@123.com>
2024-02-20 17:45:44 +0800

    feat: useUpdate自定义hooks

--- custom-hooks/src/App2.tsx
+++ custom-hooks/src/App2.tsx
@@ -1,19 +1,25 @@
 import { useState } from "react";
-import { useMount, useUnmount, useUnmountedRef } from "./hooks";
+import { useMount, useUnmount, useUnmountedRef, useSafeState } from "./hooks";
 
 import { Button, message } from "antd";
 
 const Child = () => {
   const unmountedRef = useUnmountedRef(); // 创建一个不可变的ref，用于记录组件是否卸载
+  const [count, setCount] = useSafeState(0);
   useMount(() => {
     message.info("首次渲染" + unmountedRef.current);
   });
 
   useUnmount(() => {
     message.info("组件已卸载" + unmountedRef.current);
+    console.log("组件已卸载" + count);
   });
 
-  return <div>大家好,我是小杜杜,一起玩转Hooks吧!</div>;
+  return (
+    <div onClick={() => setCount(count + 1)}>
+      大家好,我是{count},一起玩转Hooks吧!
+    </div>
+  );
 };
 
 const Index = () => {
--- custom-hooks/src/hooks/index.ts
+++ custom-hooks/src/hooks/index.ts
@@ -3,4 +3,12 @@ import useMount from "./useMount";
 import useUnmount from "./useUnmount";
 import useUnmountedRef from "./useUnmountedRef";
 import useSafeState from "./useSafeState";
-export { useLatest, useMount, useUnmount, useUnmountedRef, useSafeState };
+import useUpdate from "./useUpdate";
+export {
+  useLatest,
+  useMount,
+  useUnmount,
+  useUnmountedRef,
+  useSafeState,
+  useUpdate,
+};
