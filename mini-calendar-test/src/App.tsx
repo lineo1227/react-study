f19851141cdc0e02ddd4fe8dcd3dbc82cc2feb95
wyz <wyz@123.com>
2024-02-20 15:26:39 +0800

    feat: mini-calendar

--- mini-calendar-test/src/App.tsx
+++ mini-calendar-test/src/App.tsx
@@ -1,4 +1,10 @@
-import React, { useState } from "react";
+import React, {
+  forwardRef,
+  useImperativeHandle,
+  useState,
+  useRef,
+  useEffect,
+} from "react";
 import "./App.css";
 const monthNames = [
   "一月",
@@ -18,9 +24,26 @@ interface CalendarProps {
   value?: Date;
   onChange?: (date: Date) => void;
 }
-const Calendar: React.FC<CalendarProps> = (props) => {
+interface CalendarRef {
+  getDate: () => Date;
+  setDate: (date: Date) => void;
+}
+const InternalCalendar: React.ForwardRefRenderFunction<
+  CalendarRef,
+  CalendarProps
+> = (props, ref) => {
   const { value = new Date(), onChange } = props;
   const [date, setDate] = useState(value);
+  useImperativeHandle(ref, () => {
+    return {
+      getDate() {
+        return date;
+      },
+      setDate(date: Date) {
+        setDate(date);
+      },
+    };
+  });
   const handlePrevMonth = () => {
     setDate(new Date(date.getFullYear(), date.getMonth() - 1));
   };
@@ -38,18 +61,45 @@ const Calendar: React.FC<CalendarProps> = (props) => {
     const days = [];
     const daysCount = daysOfMonth(date.getFullYear(), date.getMonth());
     const firstDay = firstDayOfMonth(date.getFullYear(), date.getMonth());
-    for (let i = 0; i < firstDay; i++) {
-      days.push(<div key={`empty-${i}`} className="empty"></div>);
-    }
-    for (let i = 0; i < daysCount; i++) {
+    for (let i = firstDay; i > 0; i--) {
       days.push(
-        <div key={`day-${i}`} className="day">
-          {i + 1}
+        <div key={`prev-${i}`} className="empty">
+          {new Date(date.getFullYear(), date.getMonth(), -i + 1).getDate()}
+        </div>
+      );
+    }
+    for (let i = 1; i <= daysCount; i++) {
+      const clickHandler = onChange?.bind(
+        null,
+        new Date(date.getFullYear(), date.getMonth(), i)
+      );
+
+      if (i === date.getDate()) {
+        console.log(i);
+        days.push(
+          <div key={`day-${i}`} onClick={clickHandler} className="day selected">
+            {i}
+          </div>
+        );
+      } else {
+        days.push(
+          <div key={`day-${i}`} onClick={clickHandler} className="day">
+            {i}
+          </div>
+        );
+      }
+    }
+    console.log(42 - daysCount - firstDay);
+    for (let i = 1; i <= 42 - daysCount - firstDay; i++) {
+      days.push(
+        <div key={`next-${i}`} className="empty">
+          {new Date(date.getFullYear(), date.getMonth(), i).getDate()}
         </div>
       );
     }
     return days;
   };
+
   return (
     <div className="calendar">
       <div className="header">
@@ -60,23 +110,41 @@ const Calendar: React.FC<CalendarProps> = (props) => {
         <button onClick={handleNextMonth}>&gt;</button>
       </div>
       <div className="days">
-        <div className="day">日</div>
-        <div className="day">一</div>
-        <div className="day">二</div>
-        <div className="day">三</div>
-        <div className="day">四</div>
-        <div className="day">五</div>
-        <div className="day">六</div>
+        <div className="week">日</div>
+        <div className="week">一</div>
+        <div className="week">二</div>
+        <div className="week">三</div>
+        <div className="week">四</div>
+        <div className="week">五</div>
+        <div className="week">六</div>
         {renderDays()}
       </div>
     </div>
   );
 };
+const Calendar = forwardRef(InternalCalendar);
 function App() {
+  const calendarRef = useRef<CalendarRef>(null);
+  useEffect(() => {
+    console.log(calendarRef.current?.getDate().toLocaleDateString());
+    setTimeout(() => {
+      calendarRef.current?.setDate(new Date("2022-3-22"));
+    }, 3000);
+  }, []);
   return (
     <div className="App">
-      <Calendar />
-      <Calendar value={new Date("2022-2-22")} />
+      {/* <Calendar
+        onChange={(date) => {
+          alert(date.toLocaleDateString());
+        }}
+      /> */}
+      <Calendar
+        ref={calendarRef}
+        onChange={(date) => {
+          console.log(date.toLocaleDateString());
+        }}
+        value={new Date("2022-2-22")}
+      />
     </div>
   );
 }
